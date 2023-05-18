33b19be4b70dcbb7735abd4887cf2c9f1dcf4675
parksboo <parksb08@naver.com>
2023-05-18 17:34:43 +0900

    second commit

--- dataStructure_experiment/avlTree.c
+++ dataStructure_experiment/avlTree.c
@@ -31,7 +31,7 @@ Node *minValueNode(Node *node)
 {
     Node *current = node;
 
-    /* loop down to find the leftmost leaf */
+    // 가장 작은 node 찾기
     while (current->left != NULL)
         current = current->left;
 
@@ -69,7 +69,7 @@ int getBalance(Node *N)
 
 Node *insertNode(Node *node, int code, char name[50], int mk_capitalization, int price)
 {
-    /* 1. Perform the normal BST rotation */
+    // 재귀적으로 값 insertion
     if (node == NULL)
         return (newNode(code, name, mk_capitalization, price));
 
@@ -77,17 +77,14 @@ Node *insertNode(Node *node, int code, char name[50], int mk_capitalization, int
         node->left = insertNode(node->left, code, name, mk_capitalization, price);
     else if (code > node->code)
         node->right = insertNode(node->right, code, name, mk_capitalization, price);
-    else // Equal codes are not allowed in BST
+    else
         return node;
 
-    /* 2. Update height of this ancestor node */
     node->height = 1 + max(height(node->left), height(node->right));
 
-    /* 3. Get the balance factor of this ancestor node to check whether
-       this node became unbalanced */
     int BF = getBalance(node);
 
-    // If this node becomes unbalanced, then there are 4 cases
+    // balance 재조정
 
     // Left Left Case
     if (BF > 1 && code < node->left->code)
@@ -111,7 +108,6 @@ Node *insertNode(Node *node, int code, char name[50], int mk_capitalization, int
         return leftRotate(node);
     }
 
-    /* return the (unchanged) node pointer */
     return node;
 }
 
@@ -130,48 +126,46 @@ Node *deleteNode(Node *root, int code)
 
     else
     {
-        // node with only one child or no child
+        // child node < 2
         if ((root->left == NULL) || (root->right == NULL))
         {
             Node *temp = root->left ? root->left : root->right;
 
-            // No child case
             if (temp == NULL)
             {
                 temp = root;
                 root = NULL;
             }
-            else               // One child case
-                *root = *temp; // Copy the contents of the non-empty child
+            else
+                // node 당겨오기
+                *root = *temp;
 
             free(temp);
         }
         else
         {
-            // node with two children: get the inorder successor (smallest
-            // in the right subtree)
+            // left right 모두 있는 경우
             Node *temp = minValueNode(root->right);
 
-            // copy the inorder successor's data to this node
             root->code = temp->code;
+            root->mk_capitalization = temp->mk_capitalization;
+            root->price = temp->price;
 
             // delete the inorder successor
             root->right = deleteNode(root->right, temp->code);
         }
     }
 
-    // if the tree had only one node then return
+    // 빈트리가 되었는지 확인
     if (root == NULL)
         return root;
 
-    // Update height of the current node
+    // height 갱신
     root->height = 1 + max(height(root->left), height(root->right));
 
-    // Get the balance factor of this node to check whether
-    // this node became unbalanced
     int BF = getBalance(root);
 
-    // If this node becomes unbalanced, then there are 4 cases
+    // balance 재조정
 
     // Left Left Case
     if (BF > 1 && getBalance(root->left) >= 0)
@@ -197,6 +191,7 @@ Node *deleteNode(Node *root, int code)
 
     return root;
 }
+// 할당되었던 메모리를 반환하는 함수
 void freeNode(Node *node)
 {
     if (node != NULL)
@@ -215,6 +210,7 @@ void findAndPrintNode(Node *root, int code)
         // 현재 노드의 code가 찾는 code와 일치하면 노드 출력
         if (code == root->code)
         {
+            // 찾은 node를 출력하고 싶을 때 활성화
             // printf("Code: %d\n", root->code);
             // printf("Name: %s\n", root->name);
             // printf("Market capitalization: %d\n", root->mk_capitalization);
@@ -231,5 +227,5 @@ void findAndPrintNode(Node *root, int code)
     }
 
     // 노드를 찾지 못하면 메시지 출력
-    printf("No node with code: %d\n", code);
+    printf("couldn't find code: %d\n", code);
 }
